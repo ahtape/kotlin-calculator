8e825946a7e78a47cdb45d648c014c8d6565ef1c
ahtape <athasi.kurus@gmail.com>
2024-01-03 00:40:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,9 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkBlack = Color(0x010101)
+val Black = Color(0x121212)
+val DarkGrey = Color(0x5E5E5E)
+val Grey = Color(0xC2C2C2)
+val Red = Color(0xD53934)
